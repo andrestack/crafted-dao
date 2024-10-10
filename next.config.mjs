5e535124014d15923738c66fc521e06d74860f5d
André Silva <andre.silva@posteo.de>
2024-10-10 10:28:37 +1000

    fix: try workaround for sentry error

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ export default withSentryConfig(withSentryConfig(nextConfig, {
 // https://github.com/getsentry/sentry-webpack-plugin#options
 
 org: "hinterland-web",
-project: "javascript-nextjs",
+project: "crafted-dao",
 
 // Only print logs for uploading source maps in CI
 silent: !process.env.CI,
--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,13 +1,21 @@
-import * as Sentry from '@sentry/nextjs';
+import * as Sentry from "@sentry/nextjs";
 
 export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    await import('../sentry.server.config');
-  }
+  try {
+    if (process.env.NEXT_RUNTIME === "nodejs") {
+      await import("../sentry.server.config");
+    }
 
-  if (process.env.NEXT_RUNTIME === 'edge') {
-    await import('../sentry.edge.config');
+    if (process.env.NEXT_RUNTIME === "edge") {
+      await import("../sentry.edge.config");
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      error = new Error("Failed to register Sentry:", error);
+    }
+    console.error(error);
   }
+  Sentry.captureException(error);
 }
 
 export const onRequestError = Sentry.captureRequestError;
